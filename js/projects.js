fc5992dc6f5f328a33d3e858ce83316475bce9c7
Gawali, Ashish <Ashish.Gawali@analog.com>
2025-12-12 17:35:00 -0500

    Refactored content, added ADI experience, updated resume
    Refactored out the content from index.html to indivdual files for easy maintainance. Updated my resume to be latest. Added ADI experience. Remvoed Twitter handle.
    
    Signed-off-by: Gawali, Ashish <Ashish.Gawali@analog.com>

---
+++ js/projects.js
@@ -0,0 +1,51 @@
+// Projects data for portfolio
+const projectsData = [
+    {
+        id: "modal-04",
+        title: "Emotion Support Robot",
+        category: "ROS, Robotics, SLAM",
+        thumbnail: "images/portfolio/EmotionSupportRobot.gif",
+        thumbnailRetina: "images/portfolio/EmotionSupportRobot.gif",
+        modalImage: "images/portfolio/EmotionSupportRobot.gif",
+        description: "As part of the curriculum of Advanced Mobile Robotics developed a Emotion Support Robot along with Christof Fehrman, Tyler Haden and Pravardhan Reddy. Developed a Autonomous Emotional Support Robot using a ROSbot2.0 implementing Human-Robot Interaction. Devised the algorithm for the functionality of the robot. Aided in Human Detection and Human Emotion Recognition, Map creation using SLAM, and obstacle aviodance. Created simulation for the same in MATLAB and implemented it using ROS.",
+        tags: ["RTOS", "Embedded Systems", "Sensor Integration"],
+        link: "https://www.youtube.com/watch?v=HsbP6vhYnV8&ab_channel=AshishGawali",
+        linkText: "Demo Video"
+    },
+    {
+        id: "modal-03",
+        title: "Cube Game using RTOS",
+        category: "RTOS, Embedded Systems",
+        thumbnail: "images/portfolio/CubeGameAdvEmbedded.png",
+        thumbnailRetina: "images/portfolio/CubeGameAdvEmbedded.png",
+        modalImage: "images/portfolio/CubeGameAdvEmbedded.png",
+        description: "As part of the curriculum of Advanced Embedded Computing Systems developed a Cube Game along with Devin Gardner, Yudel Martinez and Kendall Livesay. My responsibilities for the project were designing and implemtation of cube motion, map creation, cursor motion, cube generation, and level progression keeping in mind the various deadlock conditions.",
+        tags: ["RTOS", "Embedded Systems", "Sensor Integration"],
+        link: "https://www.youtube.com/watch?v=9obvZ74baoM",
+        linkText: "Demo Video"
+    },
+    {
+        id: "modal-01",
+        title: "Head Motion Capture and Mimicking",
+        category: "Robotics Project",
+        thumbnail: "images/portfolio/BtechProjectBotImage2.png",
+        thumbnailRetina: "images/portfolio/BtechProjectBotImage2.png",
+        modalImage: "images/portfolio/gallery/g-BtechProjectBotImage2_2.png",
+        description: "I along with Havish Bychapur and Siddhant Kukade developed the Head Motion Capture and Mimicking as part of our Capstone Project. My responsibilities in the project were to design the PCB used. I also contributed to the code for the system.",
+        tags: ["Robotics", "PCB Design", "Embedded Systems"],
+        link: "https://drive.google.com/file/d/1gsYjRGeMhOLpSu3LRvrgn_4I0CAB58Zd/view?usp=sharing",
+        linkText: "Demo Video"
+    },
+    {
+        id: "modal-02",
+        title: "Shuttle Delivery Robot",
+        category: "Robocon 2018",
+        thumbnail: "images/portfolio/ManualBotResize.jpg",
+        thumbnailRetina: "images/portfolio/ManualBotResize@2x.jpg",
+        modalImage: "images/portfolio/gallery/g-ManualBot.jpg",
+        description: "The Shuttle Delivery Robot was developed as part of the ABU Robocon 2018. My responsibilities were to develop all of the electronic systems necessary to build the bot. The version that made it to the competetion was version 3. Each version had different actuators and approach. I designed the circuit boards necessary as well as wrote the code which was responsible to control the bot.",
+        tags: ["PCB Design", "Embedded Systems", "C"],
+        link: null,
+        linkText: null
+    }
+];
